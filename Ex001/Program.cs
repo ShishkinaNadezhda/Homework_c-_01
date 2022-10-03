ee1e1a812b17f9cc7e510ec4c67c85efb3cc272a
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-03 19:53:49 +0300

    Ex001 done

--- Ex001/Program.cs
+++ Ex001/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое число: ");
+string userInput1 = Console.ReadLine() ?? "";
+int userNumber1 = int.Parse(userInput1);
+
+Console.Write("Введите второе число: ");
+string userInput2 = Console.ReadLine() ?? "";
+int userNumber2 = int.Parse(userInput2);
+
+if (userNumber1 > userNumber2)
+{
+    Console.Write("{0} Наибольшее число", userInput1);
+}
+else
+{
+    Console.Write("{0} Наибольшее число", userInput2);
+}
+
+Console.ReadLine();
