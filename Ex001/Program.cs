e5c7e3c1ee0d68feab38a6819f487de919134ff4
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-03 20:08:09 +0300

    Ex001 edits

--- Ex001/Program.cs
+++ Ex001/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.Write("Введите первое число: ");
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Write("Введите первое число: ");
 string userInput1 = Console.ReadLine() ?? "";
 int userNumber1 = int.Parse(userInput1);
 
@@ -8,11 +9,15 @@ int userNumber2 = int.Parse(userInput2);
 
 if (userNumber1 > userNumber2)
 {
-    Console.Write("{0} Наибольшее число", userInput1);
+    Console.Write("{0} - наибольшее число, ", userInput1);
+
+    Console.Write("{0} - наименьшее число. ", userInput2);
 }
 else
 {
-    Console.Write("{0} Наибольшее число", userInput2);
+    Console.Write("{0} - наибольшее число, ", userInput2);
+
+    Console.Write("{0} - наименьшее число. ", userInput1);
 }
 
 Console.ReadLine();
