03291a77a1f3d265ff4e30349aa1bfd4f80c7dec
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-04 21:04:52 +0300

    Ex006_Week

---
+++ Ex006_Week/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача про дни недели. При вводе порядкового числа дня недели выводить название дня недели.
+Console.Write("Введите номер дня недели: ");
+string userInput = Console.ReadLine() ?? "";
+int userNumber = int.Parse(userInput);
+
+if(userNumber == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+if(userNumber == 2)
+{
+    Console.WriteLine("Вторник");
+}
+if(userNumber == 3)
+{
+    Console.WriteLine("Среда");
+}
+if(userNumber == 4)
+{
+    Console.WriteLine("Четверг");
+}
+if(userNumber == 5)
+{
+    Console.WriteLine("Пятница");
+}
+if(userNumber == 6)
+{
+    Console.WriteLine("Суббота");
+}
+if(userNumber == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+if(userNumber > 7)
+{
+    Console.Write("В неделе всего семь дней");
+}
