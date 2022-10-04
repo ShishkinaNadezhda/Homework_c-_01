d5be0a799b9de6b0c07cdaf7a194609d2608f796
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-04 18:13:05 +0300

    changed x001

--- Ex001/Program.cs
+++ Ex001/Program.cs
@@ -10,13 +10,11 @@ int userNumber2 = int.Parse(userInput2);
 if (userNumber1 > userNumber2)
 {
     Console.Write("{0} - наибольшее число, ", userInput1);
-
     Console.Write("{0} - наименьшее число. ", userInput2);
 }
 else
 {
     Console.Write("{0} - наибольшее число, ", userInput2);
-
     Console.Write("{0} - наименьшее число. ", userInput1);
 }
 
