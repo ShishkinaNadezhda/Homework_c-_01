6db32570893a6e9063030394bd17e45fd64741d6
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-09 21:17:49 +0300

    ex015_01

---
+++ Ex015_01/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа
+//  или сообщает, что третьей цифры нет.
+
+int num = Convert.ToInt32(Console.ReadLine());
+string numberText = Convert.ToString(num);
+
+if(numberText.Length > 2)
+{
+    Console.WriteLine($"Третья цифра {numberText[2]}");
+}
+
+else
+{
+    Console.WriteLine($"У числа {num} нет третьей цифры");
+}
