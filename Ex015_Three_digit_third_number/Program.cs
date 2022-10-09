a4c1508a9bfc403575d648421410cb9f46f21982
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-09 17:43:13 +0300

    Ex015_Tree_digit_third_number

--- Ex015_Three_digit_third_number/Program.cs
+++ Ex015_Three_digit_third_number/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Напишите программу, которая выводит третью цифру заданного числа
 //  или сообщает, что третьей цифры нет.
 
-int num = new Random().Next(9, 1000);
+int num = new Random().Next(9, 10000);
 Console.WriteLine($"Случайное число  {num}");
 
 if(num < 100)
@@ -9,10 +9,17 @@ if(num < 100)
     Console.WriteLine($"У числа {num} нет третьей цифры");
 }
 
-if(num > 99)
+if(num > 99 & num < 1000)
 {
     int a2_1 = num % 100;
     int a2 = a2_1 / 10;
     int a3 = a2_1 % 10;
     Console.WriteLine($"Третья цифра случайного числа  {a3}");
+}
+
+if(num > 999)
+{
+    int a3_1 = num % 100;
+    int a3_2 = a3_1 / 10;
+    Console.WriteLine($"Третья цифра случайного числа  {a3_2}");
 }
