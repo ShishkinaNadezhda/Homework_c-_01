3c065fffa1e18bdccb6eb24fea9fb59240d2fb0a
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-09 13:49:48 +0300

    ex013_1_Var2

---
+++ Ex013_1_Var2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу,которая будет принимать на вход два числа и выводить,
+// является ли второе число кратным первому.
+// Если второе число не кратно первому, то программа выводит остаток от деления.
+
+int EnterNumber()
+{
+        int number;
+        while(true)
+        {
+            Console.Write("Введите число: ");
+            if(int.TryParse(Console.ReadLine(), out number));
+                break;
+            Console.WriteLine($"Ошибка ввода!");
+        }
+        return number;
+}
+
+int FirstNumber = EnterNumber();
+int SecondNumber = EnterNumber();
+
+Console.Write($"{FirstNumber}, {SecondNumber} -> ");
+if(FirstNumber % SecondNumber == 0)
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.WriteLine($"Не кратно, остаток {FirstNumber % SecondNumber}");
+}
