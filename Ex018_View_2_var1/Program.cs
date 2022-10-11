85bdbc527bad1c6943ff71c08992c7d2976b975f
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-11 19:13:12 +0300

    Ex018_View_2_var1

---
+++ Ex018_View_2_var1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Метод, вид 2, второй вариант
+void Method2(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        i++;
+        Console.WriteLine(msg);
+    }
+}
+
+Method2(count: 4, msg: "Новый текст");
+// или Method2("Текст", 4);
+// или Method2(msg:"Текст", count: 4);
+// или Method2(count: 4, msg: "Новый текст");
+// если зациклилось, нажимаем Ctrl+C
