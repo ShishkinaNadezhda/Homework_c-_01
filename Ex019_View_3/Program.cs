347cf5832093f3ddbebcb5874faefd72cc159c3f
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-11 19:18:50 +0300

    Ex019_View_3

---
+++ Ex019_View_3/Program.cs
@@ -0,0 +1,9 @@
+﻿// Метод вид 3 - ничего не принимает, что-то возвращает/
+// Например, эта программа выводит текущий год.
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+
+int year = Method3();
+Console.WriteLine(year);
