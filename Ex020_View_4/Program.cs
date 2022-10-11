35761811151e6a91a01a446c27fe1c1d9d688c8e
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-11 19:38:28 +0300

    Ex020_View_4

---
+++ Ex020_View_4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Метод вид 4 - что-то принимает, что-то возвращает, 
+// самый распространенный метод
+
+string Method4(int count, string c)
+{
+    int i = 0;
+    string result = "";
+    // или string result = String.Empty;
+    while (i < count)
+    {
+        result = result + c;
+        i++;
+    }
+    return result;
+}
+string res = Method4(10, "asdf");
+Console.WriteLine(res);
