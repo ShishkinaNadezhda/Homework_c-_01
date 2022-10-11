be8c9e4d7965ecfb79750110f27045552af2b1eb
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-11 19:52:25 +0300

    Ex021_Function_for

---
+++ Ex021_Function_for/Program.cs
@@ -0,0 +1,18 @@
+﻿// Цикл for
+for(int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+}
+
+string Method4(int count, string text)
+{
+    string result = string.Empty;
+    for(int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+string res = Method4(10, "2");
+Console.WriteLine(res);
