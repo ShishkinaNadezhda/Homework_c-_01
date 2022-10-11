580bbc3a5b5c2d8e33aa0e9550d3fe2309738f65
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-11 19:58:05 +0300

    Ex022_for_inside_for

---
+++ Ex022_fot_inside_for/Program.cs
@@ -0,0 +1,10 @@
+﻿// Цикл можно использовать внутри цикла
+for(int i = 0; i < 10; i++)
+{
+    for(int j = 0; j < 10; j++)
+    {
+        Console.WriteLine(i*j);
+    }
+    Console.WriteLine();
+}
+//прикольно :)
