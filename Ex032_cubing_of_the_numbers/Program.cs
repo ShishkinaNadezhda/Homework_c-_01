3490509efc6e6b574322007a9860ef4f73d06372
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-13 20:34:58 +0300

    Ex032_cubing_of_the_numbers

---
+++ Ex032_cubing_of_the_numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Введите N ");
+int N = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write($"{N} -> ");
+PrintCube(N);
+
+static void PrintCube(int N)
+    {
+        for(int i = 1; i < N; i++)
+        {
+            Console.Write($"{i * i * i}, ");
+        }
+            }
+Console.Write($"{N * N * N} ");
