8ddc69d2712c846c0d7c4ecd2683d1a1951e7f4e
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-13 19:23:44 +0300

    Ex029_square_of_the_number

---
+++ Ex029_square of the number/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число N
+// и выдает таблицу квадратов числа от 1 до N
+// 5 -> 1, 5, 25
+// 2 -> 1, 4
+
+Console.Write("Введите N ");
+int N = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write($"{N} -> ");
+PrintSquares(N);
+
+/////////////////////////////////////
+static void PrintSquares(int N)
+    {
+        for(int i = 1; i < N; i++)
+        {
+            Console.Write($"{i * i}, ");
+        }
+            }
+Console.Write($"{N * N} ");
