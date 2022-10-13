8da299f8ffa2439c9bacfabd9f77e9273c87931e
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-13 17:46:29 +0300

    Ex027_Coordinate_range

---
+++ Ex027_coordinate range/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая по заданному номеру четверти 
+// показывает диапазон возможных координат точек в этой четверти  (x, y).
+
+int quarter;
+Console.Write("Введите номер четверти ");
+quarter = int.Parse(Console.ReadLine() ?? "");
+
+if (quarter == 1)
+{
+    Console.WriteLine("x > 0, y > 0");
+}
+else if (quarter == 2)
+{
+    Console.WriteLine("x < 0, y > 0");
+}
+else if (quarter == 3)
+{
+    Console.WriteLine("x < 0, y < 0");
+}
+else if (quarter == 4)
+{
+    Console.WriteLine("x > 0, y < 0");
+}
+else Console.Write("Укажите корректную четверть!");
