af65a8c8c4f2262ddb7bcb332cb2cc94a57e3af9
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-16 15:25:58 +0300

    Ex037_Degree

---
+++ Ex037_Degree/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите A ");
+        int A = int.Parse(Console.ReadLine() ?? "");
+
+        Console.Write("Введите B ");
+        int B = int.Parse(Console.ReadLine() ?? "");
+        int Degree = Convert.ToInt32(Math.Pow(A, B));
+        Console.WriteLine($"{A} в степени {B} = {Degree}");
+    }
+}
