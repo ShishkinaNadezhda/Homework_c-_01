e84639fd1f247fdd89447b20041fcecf28ae7690
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-16 14:14:36 +0300

    Ex035_Factorial

---
+++ Ex035_Factorial/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает N,
+// и выдает произведение от 1 до N.
+// 4 -> 24   1*2*3*4=24
+// 5 -> 120
+
+Console.WriteLine("Введите число N ");
+int N = Convert.ToInt32(Console.ReadLine());
+int Factorial = GetFactorial(N);
+
+Console.WriteLine(Factorial);
+
+static int GetFactorial(int N)
+{
+    int a = 1;
+    int Factorial = 1;
+    while (a <= N)
+    {
+        Factorial = a * Factorial;
+        a++;
+    }
+    return Factorial;
+}
