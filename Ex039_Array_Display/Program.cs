4598aef0868918515040b0c7b08efbe0d2a17491
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-16 17:14:06 +0300

    Ex039_Array_Display

---
+++ Ex039_Array_Display/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов 
+//и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+//честно списала
+
+int lenArray = ReadInt("Введите длину массива: ");
+int[] randomArray = new int[lenArray];
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(1,9);
+    Console.Write(randomArray[i] + " ");
+}
+
+
+// Функция ввода
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
