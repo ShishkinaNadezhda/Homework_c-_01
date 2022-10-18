6e14a2dbbc4222d4c27536bebe00bfc6482670b4
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-18 19:53:55 +0300

    Ex037_Degree_Cycle_var2

---
+++ Ex037_Degree_Cycle_var2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 25. Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в степень B.
+// 3, 5 -> 243
+//  2, 4 -> 16
+
+Console.Write("Введите число A ");
+int A = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число B ");
+int B = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{A} в степени {B} = {Pow(A, B)}");
+
+int Pow (int num, int rank)
+{
+    if(rank == 0) return 1;
+    int result = num;
+    for (int i = 2; i <= rank; i++)
+    {
+        result *=num;
+    }
+    return result;
+}
