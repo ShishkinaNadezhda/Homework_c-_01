81cdde129f587e409e1b8d691070f574c130466c
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-19 22:04:05 +0300

    Ex048_From_10_to_2

--- Ex048_From_10_to_2/Program.cs
+++ Ex048_From_10_to_2/Program.cs
@@ -1,8 +1,12 @@
 ﻿// Напишите программу, которая переводит десятичное число в двоичное.
 
 int number = UserInput("Введите число: ", "Ошибка!");
+int mode;
 
-TransformNumber(number);
+if (number >= 0) mode = number;
+else mode = number * -1;
+
+TransformNumber(number, mode);
 
 static int UserInput(string message, string errormessage)
 {
@@ -21,19 +25,19 @@ while(true)
     }
 }
 
-static void TransformNumber(int Input)
+static void TransformNumber(int Input, int modeinput)
 {
     int result = 0;
     int number;
     int multiplier = 1;
 
-    while (Input > 0)
+    while (modeinput > 0)
     {
-        number = Input % 2;
-        Input = Input / 2;
+        number = modeinput % 2;
+        modeinput = modeinput / 2;
         result = result + number * multiplier;
         multiplier *=10;
     }
-
-    Console.WriteLine($"Десятичное число транформируется в двоичное {result}");
+    if (Input >= 0) Console.WriteLine($"Десятичное число транформируется в двоичное {result}");
+    else Console.WriteLine($"Десятичное число транформируется в двоичное 1{result}");
 }
