3d9f7faa277b9d037a5a2c7ecd5231cd608ecca4
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-19 22:31:07 +0300

    Ex048_var1

---
+++ Ex048_var1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая переводит десятичное число в двоичное.
+
+int num = EnterNumber();
+string Finish = GetNumber(num);
+Console.Write(num + " -> " + Finish);
+
+string GetNumber(int N)
+{
+    string FinishNumber = "";
+    while (N > 0)
+    {
+        int count = N % 2;
+        FinishNumber = Convert.ToString(count) + FinishNumber;
+        N = N / 2;
+
+    }
+    return FinishNumber;
+}
+
+int EnterNumber()
+{
+    int number;
+    while(true)
+    {
+        Console.Write("Введите число: ");
+        if (int.TryParse(Console.ReadLine(), out number)) break;
+        else Console.WriteLine("Ошибка ввода!");
+    }
+    return number;
+}
