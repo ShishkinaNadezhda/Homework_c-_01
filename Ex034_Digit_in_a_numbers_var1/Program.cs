5c770af5d5db335173f837e16958ca9a87f7fb52
ShishkinaNadezhda <79129261498@yandex.ru>
2022-10-19 18:34:50 +0300

    Ex034_Digit_in_a_numbers_var1

---
+++ Ex034_Digit_in_a_numbers_var1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine($"Суммф цифр = {GetSumNums(number)}");
+
+
+int GetSumNums(int number)
+{
+    int sum = 0;
+    while(number > 0)
+        {
+            sum += number % 10;
+            number /= 10;
+        }
+    return sum;
+}
