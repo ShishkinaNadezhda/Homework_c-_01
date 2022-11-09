a35d57c63c0c2830abb32be698494a6ac61a28e1
ShishkinaNadezhda <79129261498@yandex.ru>
2022-11-09 20:24:29 +0300

    Ex057

---
+++ Ex057/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задайте двумерный массив. Найдите элементы, у которых оба индекса четные
+// и замените их значения на их же квадраты.
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите количество столбцов массива: ");
+int coluns = int.Parse(Console.ReadLine() ?? "");
+
+int[,] array = GetArray(rows, coluns);
+PrintArray(array);
+Console.WriteLine();
+ModArray(array);
+PrintArray(array);
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] GetArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = i + j;
+        }
+    }
+    return result;
+}
+
+void ModArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i+=2)
+    {
+        for (int j = 0; j < array.GetLength(1); j+=2)
+        {
+            array[i, j] *= array[i, j];
+        }
+    }
+}
