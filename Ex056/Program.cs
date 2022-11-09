8ee29325ebf4a5395dc181ad3d3379d7b6b1dced
ShishkinaNadezhda <79129261498@yandex.ru>
2022-11-09 20:05:55 +0300

    Ex055

---
+++ Ex056/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте двумерный массив размера m на n. Каждый элемент массива
+// находится по формуле A = m*n. Выведите полученный массив на экран.
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите количество столбцов массива: ");
+int coluns = int.Parse(Console.ReadLine() ?? "");
+
+int[,] array = GetArray(rows, coluns);
+PrintArray(array);
+Console.WriteLine();
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] GetArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = i + j;
+        }
+    }
+    return result;
+}
