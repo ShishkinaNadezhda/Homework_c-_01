a8b3b3ebf5a0daec3a69039768996c8eb29d1563
ShishkinaNadezhda <79129261498@yandex.ru>
2022-11-10 20:38:53 +0300

    Ex061

---
+++ Ex061/Program.cs
@@ -0,0 +1,39 @@
+﻿// написать программу, которая принимает на вход три числа и проверяет
+// может ли существовать треугольник со сторонами такой длины.
+// Каждая сторона треугольника меньше суммы двух других сторон.
+
+int a = GetNumber("Введите длину первой стороны треугольника: ", "Ошибка ввода!");
+int b = GetNumber("Введите длину второй стороны треугольника: ", "Ошибка ввода!");
+int c = GetNumber("Введите длину третьей стороны треугольника: ", "Ошибка ввода!");
+
+Triangle(a, b, c);
+
+
+static int GetNumber(string message, string errormessage)
+{
+while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int result);
+        if (isCorrect)
+        {
+            return result;
+        }
+        else
+        {
+            Console.WriteLine(errormessage);
+        }
+    }
+}
+
+void Triangle(int a, int b, int c)
+{
+    if (a < (b + c) && b < (a + c) && c < (a + b))
+    {
+        Console.Write("Такой треугольник может существовать");
+    }
+    else
+    {
+        Console.Write("Такой треугольник не может существовать");
+    }
+}
