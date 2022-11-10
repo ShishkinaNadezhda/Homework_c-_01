cb4278937c93d9e687b469b60875516f73c64591
ShishkinaNadezhda <79129261498@yandex.ru>
2022-11-10 19:09:40 +0300

    Ex059

---
+++ Ex059/Program.cs
@@ -0,0 +1,17 @@
+﻿// Считает сумму цифр в числе, введенном с консоли.
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()?? "");
+Console.WriteLine($"Сумма цифр = {GetSumNums(num)}");
+
+
+int GetSumNums(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum+=number%10;
+        number/=10;
+    }
+        return sum;
+}
