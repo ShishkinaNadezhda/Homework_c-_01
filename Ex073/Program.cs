1029a7c83fc92245b588a324c8d50a4bc9b94e31
ShishkinaNadezhda <79129261498@yandex.ru>
2022-11-15 20:30:16 +0300

    Ex073

---
+++ Ex073/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход будет принимать число
+// и выдавать сумму его цифр.
+
+Console.Write($"Введите целое число N: ");
+int n = int.Parse(Console.ReadLine() ?? "");
+
+Console.WriteLine($"{SumDigits(n)}");
+
+int SumDigits (int number)
+{
+    if (number == 0)
+        return 0;
+    return SumDigits(number/10) + number%10;
+}
