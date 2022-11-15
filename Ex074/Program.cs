8867c97d2e81c336bc47527c93c0dd4a5c9efc40
ShishkinaNadezhda <79129261498@yandex.ru>
2022-11-15 20:37:39 +0300

    Ex074

---
+++ Ex074/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимать два числа - A и B,
+// и возводит число A в целую степень B с помощью рекурсии.
+
+Console.Write($"Введите целое число A: ");
+int a = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write($"Введите целое число B: ");
+int b = int.Parse(Console.ReadLine() ?? "");
+
+Console.WriteLine($"{PowerNumber(a, b)}");
+
+int PowerNumber (int a, int b)
+{
+    if (b == 0) return 1;
+    return a * PowerNumber(a, b-1);
+}
