409969bed94e2dfc43599a24dc8a7727aa4eb67a
ShishkinaNadezhda <79129261498@yandex.ru>
2022-11-15 19:54:07 +0300

    Ex072

--- Ex072/Program.cs
+++ Ex072/Program.cs
@@ -1,23 +1,31 @@
 ﻿// Задайте значение M и N. Напишите программу, которая выведет все натуральные числа от M до N.
 // N = 5 -> 1, 2, 3, 4, 5
 
-Console.WriteLine($"Введите целое число N: ");
-int n = int.Parse(Console.ReadLine() ?? "");
 
-Console.WriteLine($"Введите целое число M: ");
-int m = int.Parse(Console.ReadLine() ?? "");
+Console.Clear();
+int num1 = GetNumberFromUser("Введите число M: ", "Ошибка ввода!");
+int num2 = GetNumberFromUser("Введите число N: ", "Ошибка ввода!");
 
-Console.WriteLine(PrintNaturals(m, n));
+if (num1 < num2)
+    {
+        Console.WriteLine(PrintNaturals(num1, num2));
+    }
+    else
+    {
+        Console.WriteLine($"Вывод невозможен, {num1} больше {num2}");
+    }
 
-if (m < n)
+int GetNumberFromUser(string message, string errorMessage)
 {
-    Console.WriteLine($"{PrintNaturals(m, n)}");
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+            Console.WriteLine(errorMessage);
+    }
 }
-else
-{
-    Console.WriteLine("Вывод невозможен!");
-}
-
 
 string PrintNaturals(int start, int end)
 {
@@ -25,6 +33,5 @@ string PrintNaturals(int start, int end)
     {
         return start.ToString();
     }
-
-return (start + ", " + PrintNaturals(start + 1, end));
+    return (start + ", " + PrintNaturals(start + 1, end));
 }
