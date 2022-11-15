18d59c9a76b71a09a47be7ea5778ff55a400911b
ShishkinaNadezhda <79129261498@yandex.ru>
2022-11-15 19:53:16 +0300

    Ex071

--- Ex071/Program.cs
+++ Ex071/Program.cs
@@ -1,12 +1,12 @@
 ﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа от 1 до N.
 // N = 5 -> 1, 2, 3, 4, 5
 
-Console.WriteLine($"Введите целое число N: ");
+Console.Write($"Введите целое число N: ");
 
 int n = int.Parse(Console.ReadLine() ?? "");
 Console.WriteLine(PrintNaturals(1, n));
 
-static string PrintNaturals(int start, int end)
+string PrintNaturals(int start, int end)
 {
     if (start == end)
     {
