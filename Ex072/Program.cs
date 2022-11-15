2b9bf01883f977a5c233c79426038dc08511d78a
ShishkinaNadezhda <79129261498@yandex.ru>
2022-11-15 19:44:42 +0300

    Ex072

---
+++ Ex072/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте значение M и N. Напишите программу, которая выведет все натуральные числа от M до N.
+// N = 5 -> 1, 2, 3, 4, 5
+
+Console.WriteLine($"Введите целое число N: ");
+int n = int.Parse(Console.ReadLine() ?? "");
+
+Console.WriteLine($"Введите целое число M: ");
+int m = int.Parse(Console.ReadLine() ?? "");
+
+Console.WriteLine(PrintNaturals(m, n));
+
+if (m < n)
+{
+    Console.WriteLine($"{PrintNaturals(m, n)}");
+}
+else
+{
+    Console.WriteLine("Вывод невозможен!");
+}
+
+
+string PrintNaturals(int start, int end)
+{
+    if (start == end)
+    {
+        return start.ToString();
+    }
+
+return (start + ", " + PrintNaturals(start + 1, end));
+}
